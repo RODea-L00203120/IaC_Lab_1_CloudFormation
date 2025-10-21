dc4597f0fd89890aef2b6c9b37becec7bb2d9053
RODea_L00203120 <L00203120@atu.ie>
2025-10-21 21:38:56 +0100

    reverted function to original

--- _Experimental/lambda_function.py
+++ _Experimental/lambda_function.py
@@ -1,14 +1,3 @@
-import json
-
+import sys
 def handler(event, context):
-    # This code is designed to be triggered by API Gateway
-    # It gets the source IP from the 'requestContext'
-    ip_address = event['requestContext']['identity']['sourceIp']
-
-    return {
-        'body': json.dumps({"ip_address": ip_address}),
-        'headers': {
-            'Content-Type': 'application/json'
-        },
-        'statusCode': 200
-    }
+    return 'Hello from AWS Lambda using Python' + sys.version + '!'
