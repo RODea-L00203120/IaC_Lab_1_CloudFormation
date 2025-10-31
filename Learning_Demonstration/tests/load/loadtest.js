33cd4378ba0a80c5beb9ec5ee97fd36eeecd8e71
RODea_L00203120 <L00203120@atu.ie>
2025-10-31 10:43:39 +0000

    Added metric, log, trace examples to Learning_Demonstration/Steps_Taken.md

--- Learning_Demonstration/tests/load/loadtest.js
+++ Learning_Demonstration/tests/load/loadtest.js
@@ -1,6 +1,7 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
+// list of random names - pool to be selected from by Math.random
 const names = [
   'Alice', 'Bob', 'Charlie', 'David', 'Emma',
   'Frank', 'Grace', 'Henry', 'Iris', 'Jack',
@@ -14,8 +15,10 @@ const names = [
   'Val', 'Will', 'Xena', 'Yogi', 'Zoe'
 ];
 
+// Here is the base endpoint for the live AWS deployment
 const BASE_URL = 'https://sgeb81br8a.execute-api.us-east-1.amazonaws.com/Prod';
 
+// One virtual user running 50 requests - easy to scale but beware of cost
 export let options = {
   vus: 1,          // 1 virtual user
   iterations: 50,  // 50 total requests
@@ -28,6 +31,7 @@ export default function () {
   // Test /greeting/{name}
   const res = http.get(`${BASE_URL}/greeting/${randomName}`);
   
+  // k6 true/false assertation based test - pulls data form event json data
   check(res, {
     'status is 200': (r) => r.status === 200,
     'has message': (r) => r.json('message') !== undefined,
